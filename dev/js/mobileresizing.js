09273151a7706a43cc86e172797ae66dceb53546
hkapoor2001 <hkapoor2001@gmail.com>
2022-02-01 14:49:18 -0500

    fixed codekit issue

--- dev/js/mobileresizing.js
+++ dev/js/mobileresizing.js
@@ -3,7 +3,6 @@ import { gsap } from "gsap";
 export function displayWindowSize(){
 
     let menu = document.querySelector("#nav");
-
     let menuWidth = menu.offsetWidth;
 
     if(document.documentElement.clientWidth <= 1024){
--- dev/js/pageScroll-min.js
+++
@@ -1,1 +0,0 @@
-import gsap from"gsap";import{ScrollToPlugin}from"gsap/ScrollToPlugin";gsap.registerPlugin(ScrollToPlugin);let idArray=["#nav1 #nav2 #nav3 "];export function scrollPage(o){gsap.to(window,{duration:3,scrollTo:idArray[o]})}
